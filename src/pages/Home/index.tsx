27eea23cb72c69d9dbb504f7dbc41a67703aa1cf
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-14 20:54:49 -0600

    HomePage

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { HomePage } from "./styles";
+import { Chat } from "../../components/Chat";
+
+export const Home = () => {
+  return (
+    <HomePage>
+      <Chat />
+    </HomePage>
+  );
+};
