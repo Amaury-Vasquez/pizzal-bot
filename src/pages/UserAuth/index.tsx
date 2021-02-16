5a0ecf9f5b37a3a9255f51b2a10ffb5daf0c1997
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-15 18:12:36 -0600

    Auth page created

---
+++ src/pages/UserAuth/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Page } from "../../styles/elements";
+import { Auth } from "../../components/Auth";
+
+export const UserAuth = () => {
+  return (
+    <Page>
+      <Auth />
+    </Page>
+  );
+};
