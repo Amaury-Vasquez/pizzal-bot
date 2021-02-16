260a72efecc8aec015d105b0e72c99d75480ff86
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-15 18:11:45 -0600

    Context and Auth interfaces created

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,13 @@ export interface MessageValues {
   text: string;
   incoming: boolean;
 }
+
+export interface ContextValues {
+  isAuth?: boolean;
+  setAuth?: Function;
+}
+
+export interface AuthFormValues {
+  email: string;
+  password: string;
+}
