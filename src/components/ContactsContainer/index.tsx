f78ebd656acdcdcceba25e8ced8b63216c610b32
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-17 19:39:17 -0600

    Component renamed

---
+++ src/components/ContactsContainer/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Box } from "./styles";
+import { ChatContact } from "../ChatContact";
+import { ResponseValues } from "../../interfaces/index";
+
+export const ContactsContainer = (props: {
+  contacts: Array<ResponseValues>;
+  callback: Function;
+}) => {
+  const { contacts, callback } = props;
+  return (
+    <Box>
+      {contacts.map((item) => (
+        <ChatContact
+          onClick={callback}
+          conversation={item.conversation}
+          fbId={item.fbId}
+          id={item.id}
+          lastName={item.lastName}
+          name={item.name}
+          phone={item.phone}
+          key={`${item.fbId}+${item.name}+${item.lastName}+${item.id}+${item.phone}`}
+        />
+      ))}
+    </Box>
+  );
+};
---
+++ src/components/ContactsContainer/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  width: 25vw;
+  min-height: 100%;
+  height: auto;
+  overflow-y: scroll;
+  padding: 20px;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  border-radius: 5px;
+`;
