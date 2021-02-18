b93d88d3abf45c72721750750882fdc00d8bd72e
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-17 21:16:11 -0600

    Message props

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,26 +1,25 @@
-import React from "react";
-import { AiOutlineSend } from "react-icons/ai";
+import React from 'react';
+import { AiOutlineSend } from 'react-icons/ai';
 
-import { ChatBox, Container, InputBox, Input, SubmitButton } from "./styles";
-import { Message } from "../Message";
-import { useChatInput } from "../../hooks/useChatInput";
-import { MessageValues } from "../../interfaces";
+import { ChatBox, Container, InputBox, Input, SubmitButton } from './styles';
+import { Message } from '../Message';
+import { useChat } from '../../hooks/useChat';
+import { MessageValues } from '../../interfaces';
 
 export const Chat = (props: { activeChat: MessageValues[] }) => {
-  const { chat, input, handleChange, handleSubmit } = useChatInput(
-    props.activeChat
-  );
+  const { chat, input, handleChange, handleSubmit } = useChat(props.activeChat);
   return (
     <Container>
       <ChatBox>
         {chat.map((item) => {
-          return item.message.text !== "" ? (
+          const { message, id, timestamp, sendBy } = item;
+          return item.message.text !== '' ? (
             <Message
-              id={item.id}
-              timestamp={item.timestamp}
-              key={`${item.id}+${item.sendby}+${item.timestamp}`}
-              message={{ text: item.message.text, type: "text" }}
-              sendby={item.sendby}
+              id={id}
+              timestamp={timestamp}
+              key={`${id}+${sendBy}+${timestamp}`}
+              message={message}
+              sendBy={sendBy}
             />
           ) : null;
         })}
