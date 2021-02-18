c2d2031fe917009b2a09acf83d510318736eab48
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-17 19:43:20 -0600

    Page content modified

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+import { Container } from "./styles";
 import { Page } from "../../styles/elements";
 import { Chat } from "../../components/Chat";
+import { ContactsContainer } from "../../components/ContactsContainer";
+import { useChatsLoad } from "../../hooks/useChatsLoad";
 
 export const Home = () => {
+  const { activeChat, contacts, callback } = useChatsLoad();
+
   return (
     <Page>
-      <Chat />
+      <Container>
+        <ContactsContainer contacts={contacts} callback={callback} />
+        <Chat activeChat={activeChat} />
+      </Container>
     </Page>
   );
 };
---
+++ src/pages/Home/styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 70vh;
+  box-sizing: border-box;
+  width: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: var(--shadow);
+  background-color: var(--white);
+`;
