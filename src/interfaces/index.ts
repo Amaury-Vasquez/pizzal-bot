ca9d10a50c4d410f92bfa62b1b60b26d6fd09bf3
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-02-17 19:43:42 -0600

    Message values changed

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,8 +1,3 @@
-export interface MessageValues {
-  text: string;
-  incoming: boolean;
-}
-
 export interface ContextValues {
   isAuth?: boolean;
   setAuth?: Function;
@@ -11,4 +6,23 @@ export interface ContextValues {
 export interface AuthFormValues {
   email: string;
   password: string;
+}
+
+export interface ResponseValues {
+  conversation: { id: number; source: string };
+  fbId: string | null;
+  id: number;
+  lastName: string | null;
+  name: string | null;
+  phone: string | null;
+}
+
+export interface MessageValues {
+  id: number;
+  sendby: string;
+  timestamp: string;
+  message: {
+    text: string;
+    type: string;
+  };
 }
