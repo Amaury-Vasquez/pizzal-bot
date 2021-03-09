4723cbfb292a4b979e9035916abccc8bc980f732
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-03-09 15:28:39 -0600

    jwt, container props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Container } from './styles';
 import { Page } from '../../styles/elements';
 import { Chat } from '../../components/Chat';
 import { ContactsContainer } from '../../components/ContactsContainer';
 import { useChatsLoad } from '../../hooks/useChatsLoad';
+import AppContext from '../../Context/index';
 
 export const Home = () => {
-  const { activeChat, contacts, callback } = useChatsLoad();
-
+  const { token, url } = useContext(AppContext);
+  const { loaded, activeChat, contacts, callback } = useChatsLoad(
+    token ? token.jwt : '',
+    url,
+  );
   return (
     <Page>
       <Container>
-        <ContactsContainer contacts={contacts} callback={callback} />
+        <ContactsContainer
+          loaded={loaded}
+          contacts={contacts}
+          callback={callback}
+        />
         <Chat activeChat={activeChat} />
       </Container>
     </Page>
