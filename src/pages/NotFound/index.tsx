ff582af6953fa507b789a0fa23434d2f6e28ca61
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-03-20 17:28:43 -0600

    Page element repeated

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { Page } from './styles';
+import { Page } from '../../styles/elements';
 import { Error404 } from '../../components/Error404';
 
 export const NotFound = () => {
