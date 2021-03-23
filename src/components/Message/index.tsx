5d370a9fdaa19393a68ca121f9c5f6c281196b24
Amaury-Vasquez <amaury.vasquezenriquez@gmail.com>
2021-03-23 11:57:51 -0600

    Image messages added

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
-import { MessageBox, Text } from './styles';
+import { PopupImage } from '../PopupImage';
+import { MessageBox, Text, Image, Figure } from './styles';
 import { MessageValues } from '../../interfaces/index';
 
 export const Message = (props: MessageValues) => {
-  return (
-    <MessageBox incoming={props.sendBy === 'bot' ? true : false}>
-      <Text> {props.message.text} </Text>
-    </MessageBox>
+  const { type, url, text } = props.message;
+  const [imageFocused, setFocus] = useState(false);
+
+  const ChatImage = () => (
+    <Image
+      src={url ? url : ''}
+      alt={'chat image'}
+      onClick={() => setFocus(!imageFocused)}
+    />
   );
+  const ImageMessage = () => {
+    return (
+      <Fragment>
+        <MessageBox
+          type={type}
+          incoming={props.sendBy === 'bot' ? true : false}
+        >
+          <Figure>
+            <ChatImage />
+          </Figure>
+        </MessageBox>
+        <PopupImage
+          image={<ChatImage />}
+          active={imageFocused}
+          callback={() => setFocus(false)}
+        />
+      </Fragment>
+    );
+  };
+  const TextMessage = () => {
+    return (
+      <MessageBox type={type} incoming={props.sendBy === 'bot' ? true : false}>
+        <Text> {text ? text : ''} </Text>
+      </MessageBox>
+    );
+  };
+  return type === 'image' ? <ImageMessage /> : <TextMessage />;
 };
--- src/components/Message/styles.tsx
+++ src/components/Message/styles.tsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
-export const MessageBox = styled.div<{ incoming: boolean }>`
+export const MessageBox = styled.div<{ incoming: boolean; type: string }>`
   display: flex;
   /* justify-content: left; */
   align-items: center;
-  width: 100%;
+  ${(props) =>
+    props.type === 'image'
+      ? 'padding: 0px; width: 70%;'
+      : 'padding: 15px; width: 100%;'};
   height: auto;
   text-align: left;
-  padding: 15px;
   background-color: ${(props) =>
     props.incoming ? 'var(--gray)' : 'var(--green)'};
   margin-bottom: 1vh;
@@ -18,6 +20,25 @@ export const Text = styled.span`
   height: 100%;
   width: auto;
   line-height: 1.5em;
-  max-width: 90%;
   font-size: 16px;
+  max-width: 90%;
+`;
+
+export const Figure = styled.figure`
+  height: auto;
+  width: 100%;
+  text-align: center;
+  align-items: center;
+  box-sizing: border-box;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.5s ease;
+  }
 `;
